ee73b7e9f8d43cbe81ebc5318002de8a3f1ef517
1M0reBug <jordan.quagliatini@gmail.com>
2015-06-15 11:47:09 +0200

    angular app working

--- public/javascripts/videoCtrl.js
+++ public/javascripts/videoCtrl.js
@@ -3,5 +3,23 @@
 var videoCtrl = angular.module('videoCtrl', []);
 
 
-videoCtrl.controller('', ['$scope', '$http', function($scope, $http) {
+videoCtrl.controller('videoLoader', ['$scope', '$http', function($scope, $http) {
+
+    $scope.load = function(url) {
+        $scope.loadedVideo = { "url" : url};
+    };
+
+    $scope.videos = [
+        {
+            name : "video",
+            url : "video.mp4",
+            weight : "123kb"
+        },
+        {
+            name : "video2",
+            url : "video2.mp4",
+            weight : "alaln"
+        }
+    ];
+
 }]);
